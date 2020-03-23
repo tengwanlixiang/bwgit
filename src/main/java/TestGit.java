29376346a31251c4a8478ccf18121f3a1b6ea08c
zhanben <321422946@qq.com>
2020-03-23 08:34:38 +0800

    我改了

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,6 +2,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("北网加油 北网加油 北网加油");
         System.out.println("武汉加油 北京加油 意大利加油");
+        System.out.println("明天会更好");
     }
 
 }
